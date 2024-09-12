e6c5b3794aca459ea799f7db1fde23ae6978a139
Claudiazepam <martinezallendesclaudia@gmail.com>
2024-09-12 14:29:56 -0300

    avance ejercicio 2

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
+//Azul
+
+function pintar(color){
+cuadradoAzul = document.querySelector("#azul");
+cuadradoAzul.style.backgroundColor = color;
+}
+const btnAzul = cuadradoAzul.addEventListener("click", pintar);
+
+
+//Rojo
+function pintar(color){
+    cuadradoRojo = document.querySelector("#rojo");
+    cuadradoRojo.style.backgroundColor = color;
+}
+const btnRojo = cuadradoRojo.addEventListener("click", pintar);
+
+// Verde
+
+function pintar(color){
+cuadradoVerde = document.querySelector("#verde");
+cuadradoVerde.style.backgroundColor = color;
+}
+const btnVerde = cuadradoVerde.addEventListener("click", pintar);
+
+// Amarillo
+
+function pintar(color){
+cuadradoAmarillo = document.querySelector("#amarillo");
+cuadradoAmarillo.style.backgroundColor = color;
+}
+
+const btnAmarillo = cuadradoAmarillo.addEventListener("click", pintar);
+
+
+
+/*
+btn = document.querySelector("#azul");
+btn.addEventListener("click", pintar);
+*/
+
+
 
 
